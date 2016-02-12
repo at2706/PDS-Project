b3078204899fbc4c7fc41831429f9bb3b54f63c7
Andy Tang <andy.tang@nyu.edu>
2016-02-12 15:05:59 -0500

    Changed Template, added some pages.

--- main.py
+++ main.py
@@ -8,10 +8,28 @@ app = Flask(__name__)
 def home():
     return render_template(
         'index.html',
-        title='Contact',
-        # year=datetime.now().year,
-        message='Your contact page.'
+        title='Home'
     )
+
+
+@app.route("/register")
+def register():
+    return render_template(
+        'register.html',
+        title="Register"
+    )
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return 'You want path: %s' % path
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template(
+        '404.html',
+        title="404 Error"), 404
 
 
 app.run("127.0.0.1", 5000, debug=True)
